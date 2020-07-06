8e1792f2a140d14e36ac55587890fecc33470cc5
renemonroy <rene.monroy@gmail.com>
2020-07-06 03:36:35 -0500

    Render image as background

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
+import { IImgZoomProps } from "./index.types";
+import useLoadImage from "./utils/useLoadImage";
 
-export default function ImgZoom() {
+// https://picsum.photos/id/237/536/354
+
+export default function ImgZoom(props: IImgZoomProps): JSX.Element {
+  const { width, height, src } = props;
+
+  const [imgStatus, imgSrc] = useLoadImage(src);
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const [, setCtx] = React.useState<CanvasRenderingContext2D | null>(null);
+
+  React.useEffect((): void => {
+    if (imgStatus === "LOADED") {
+      const mainCtx = canvasRef.current!.getContext(
+        "2d",
+      ) as CanvasRenderingContext2D | null;
+      if (mainCtx != null) mainCtx.drawImage(imgSrc, 0, 0, width, height);
+      setCtx(mainCtx);
+    }
+  }, [imgStatus]);
+
+  const w = `${width}px`;
+  const h = `${height}px`;
+
   return (
-    <div className="img-zoom">
-      <p>Here is going to be the img-zoom elements</p>
+    <div className="img-zoom" style={{ width: w, height: h }}>
+      <canvas ref={canvasRef} width={w} height={h} />
     </div>
   );
 }
