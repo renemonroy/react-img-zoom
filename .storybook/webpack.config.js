753e6714a5a1ba50b8e0fbff3db02c6fe52676cc
renemonroy <rene.monroy@gmail.com>
2020-07-05 23:11:58 -0500

    Add Storybook (w/ Typescript) as dev env server

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,36 @@
+const createCompiler = require("@storybook/addon-docs/mdx-compiler-plugin");
+
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.mdx$/,
+    use: [
+      {
+        loader: "babel-loader",
+        options: {
+          presets: [["react-app", { flow: false, typescript: true }]],
+        },
+      },
+      {
+        loader: "@mdx-js/loader",
+        options: {
+          compilers: [createCompiler({})],
+        },
+      },
+    ],
+  });
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("awesome-typescript-loader"),
+      },
+      // Optional
+      {
+        loader: require.resolve("react-docgen-typescript-loader"),
+      },
+    ],
+  });
+  config.resolve.extensions.push(".ts", ".tsx", ".js", ".jsx", ".mdx");
+  return config;
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function ImgZoom() {
+  return (
+    <div className="img-zoom">
+      <p>Here is going to be the img-zoom elements</p>
+    </div>
+  );
+}
